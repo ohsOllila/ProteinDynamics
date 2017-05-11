9766f494f8746bbc05e1e9de67d8a52999af82c1
ohsOllila <samuli.ollila@helsinki.fi>
2017-05-11 16:59:08 +0300

    Updates

--- Figs/scripts/coeffsCALC.sh
+++ Figs/scripts/coeffsCALC.sh
@@ -1,4 +1,4 @@
-awk '{if($1>0) print NR" "$0}' ~/Dropbox/PsTonB/Data/coeffs331.dat | awk '{sum=sum+$2; print sum}'
-awk '{if($1>0) print NR" "$0}' ~/Dropbox/PsTonB/Data/coeffs331.dat
-awk '{if($1>0) print NR" "$0}' ~/Dropbox/TonB/Data/coeffs331.dat | awk '{sum=sum+$2; print sum}'
-awk '{if($1>0) print NR" "$0}' ~/Dropbox/TonB/Data/coeffs331.dat
+awk '{if($1>0) printf "%12.3f %12.3f %12.3f\n", NR, $1, $2}' ~/Dropbox/PsTonB/Data/coeffs331.dat | awk '{sum=sum+$2; print sum}'
+awk '{if($1>0) printf "%12.3f %12.3f %12.3f\n", NR, $1, $2}' ~/Dropbox/PsTonB/Data/coeffs331.dat
+awk '{if($1>0) printf "%12.3f %12.3f %12.3f\n", NR, $1, $2}' ~/Dropbox/TonB/Data/coeffs331.dat | awk '{sum=sum+$2; print sum}'
+awk '{if($1>0) printf "%12.3f %12.3f %12.3f\n", NR, $1, $2}' ~/Dropbox/TonB/Data/coeffs331.dat
